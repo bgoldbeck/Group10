6ea9d13870fe1fecf59a78f162354528bc7ab24c
bgoldbeck <bgoldbeck21@gmail.com>
2017-11-08 11:47:54 -0800

    Pushing ChocAnServerTests.cs up.

---
+++ test/UnitTests/ChocAnServerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using ChocAnServer;
+using ChocAnServer.Packets;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ChocAnServerTests
+    {
+        [TestMethod]
+        public void TestProcessAction()
+        {
+            ChocAnServer.ChocAnServer server = new ChocAnServer.ChocAnServer();
+            return;
+        }
+
+        [TestMethod]
+        public void TestRequestAddMember()
+        {
+            ChocAnServer.ChocAnServer server = new ChocAnServer.ChocAnServer();
+            return;
+        }
+
+        [TestMethod]
+        public void TestRequestMemberStatus()
+        {
+            ChocAnServer.ChocAnServer server = new ChocAnServer.ChocAnServer();
+
+            //server.ProcessAction(new MemberPacket(1234, "MEMBER_STATUS"));
+
+            return;
+        }
+    }
+}
