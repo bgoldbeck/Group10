cbde750211d6e655129365f51fa2a8b91762b3f5
DanielRBramblett <DanielRBramblett@users.noreply.github.com>
2017-11-07 17:14:15 -0800

    I added code for the base packet and unit tests for the base packet.

--- src/Server/Packets/BasePacket.cs
+++ src/Server/Packets/BasePacket.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/// TODO: Please add a description ;_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,45 @@ namespace ChocAnServer.Packets
 {
     public class BasePacket
     {
+        private string action;
+        private string sessionID;
+
+        /// <summary>
+        /// This is the default constructor for the base packet class to set the
+        /// fields in the class to a default value. It's also private so only this
+        /// class has access to invoke this constructor.
+        /// </summary>
+        protected BasePacket()
+        {
+            this.action = null;
+            this.sessionID = null;
+        }
+        /// <summary>
+        /// This is the constructor which takes in a string for the action and the
+        /// string for the session ID and it sets the fields.
+        /// </summary>
+        /// <param name="newAction"></param>
+        /// <param name="newSessionID"></param>
+        public BasePacket(string newAction, string newSessionID)
+        {
+            this.action = newAction;
+            this.sessionID = newSessionID;
+        }
+        /// <summary>
+        /// This method returns the string stored in the action data member.
+        /// </summary>
+        /// <returns></returns>
+        public string Action()
+        {
+            return action;
+        }
+        /// <summary>
+        /// This method returns the string stored in the session ID data member.
+        /// </summary>
+        /// <returns></returns>
+        public string SessionID()
+        {
+            return sessionID;
+        }
     }
 }
---
+++ test/UnitTests/BasePacketTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChocAnServer.Packets;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class BasePacketTests
+    {
+        [TestMethod]
+        public void TestPacketConstructor()
+        {
+            string action = "Add Member";
+            string sessionID = "1209384209385";
+            BasePacket testPacket = new BasePacket(action, sessionID);
+            Assert.AreEqual(action, testPacket.Action());
+            Assert.AreEqual(sessionID, testPacket.SessionID());
+            return;
+        }
+    }
+}
