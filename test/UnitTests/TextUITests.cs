32e19a7ecbd69cd7bb468c916b12411c09599982
bgoldbeck <bgoldbeck21@gmail.com>
2017-11-10 13:28:45 -0800

    Worked on Unit Tests for the TextUI class.
    + Added TestCurrentCursorPosition()
    + TestWriteLine() method added more assertions.

--- test/UnitTests/TextUITests.cs
+++ test/UnitTests/TextUITests.cs
@@ -2,24 +2,51 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HealthcareClientSystem;
 
+
 namespace UnitTests
 {
     [TestClass]
     public class TextUITests
     {
         [TestMethod]
+        public TextUI BuildTextUIInstance(int nRows, int nCols)
+        {
+            return new TextUI(nRows, nCols);
+        }
+
+        [TestMethod]
         public void TestWriteLine()
         {
-            int nRows = 10;
-            int nCols = 40;
-
-            TextUI ui = new TextUI(nRows, nCols);
+            TextUI ui = BuildTextUIInstance(40, 40);
 
             // The first Writeline should return the cursor position : 2.
             Assert.AreEqual(ui.WriteLine("first line added."), 2);
-            Assert.AreEqual(ui.CurrentCursorPosition(), 2);
 
             return;
         }
+
+        [TestMethod]
+        public void TestCurrentCursorPosition()
+        {
+            TextUI ui = BuildTextUIInstance(40, 40);
+
+            // Cursor line position initially is 1. (The top index is the ### header)
+            for (int i = 1; i < ui.MaximumCursorPosition(); ++i)
+            {
+                ui.WriteLine("write a line added.");
+
+                // Check each time we write a line, the cursor position gets incremented.
+                Assert.AreEqual(ui.CurrentCursorPosition(), i + 1);
+            }
+
+            ui.WriteLine("write another line added.");
+
+            // By now we should have overflowed the TextUI cursor line index. We check to make
+            // sure that happened.
+            Assert.AreEqual(ui.CurrentCursorPosition(), 1);
+
+            return;
+        }
+
     }
 }
