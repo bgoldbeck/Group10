733a11e81fd6e4f3e56496c8f9c2a86239ab5cde
River Bartz <clearflare@live.com>
2017-11-23 17:16:46 -0800

    Fixed the OperatorTerminalTests

--- test/UnitTests/OperatorTerminalTests.cs
+++ test/UnitTests/OperatorTerminalTests.cs
@@ -16,7 +16,7 @@ namespace UnitTests
         {
 
             OperatorTerminal.EnableMock();
-            OperatorTerminal operatorTerminal = new OperatorTerminal();
+            OperatorTerminal operatorTerminal = new OperatorTerminal(true);
             Assert.IsNotNull(operatorTerminal);
             OperatorTerminal.DisableMock();
             return;
@@ -29,7 +29,7 @@ namespace UnitTests
             OperatorTerminal.ChangeMockState(OperatorTerminal.TerminalState.LOGIN);
             OperatorTerminal.ChangeMockPacket(new LoginPacket("LOGIN", "", "123456789", "asdf", 1));
 
-            OperatorTerminal operatorTerminal = new OperatorTerminal();
+            OperatorTerminal operatorTerminal = new OperatorTerminal(true);
             operatorTerminal.Loop();
 
             // I set the MockPacket to a response pack when mock mode is enabled.
